99acd2df2b83f688ce5c94a7a74a957bdea1ac9d
budeikin <budeikin52@gmail.com>
2023-10-01 08:52:36 +0330

    updating detail of books page

--- core/book/models.py
+++ core/book/models.py
@@ -25,10 +25,14 @@ class Book(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     cover = models.ImageField(upload_to='books/', null=True, blank=True)
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name='books')
+    genre = models.ManyToManyField(Genre, blank=True, related_name='books')
     writer = models.ForeignKey(Writer, on_delete=models.CASCADE, related_name='books')
     release_date = models.DateTimeField(null=True, blank=True)
     leaves = models.PositiveIntegerField()
+
+    @property
+    def yearpublished(self):
+        return self.release_date.strftime('%Y')
 
     def get_absolute_url(self):
         return reverse('book:book_detail', args=[self.id])
--- core/home/views.py
+++ core/home/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
+from book.models import Writer
+
+
 # Create your views here.
 
 
 class HomeView(TemplateView):
     template_name = 'home/home_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        writer = Writer.objects.get(id=1)
+        context['books'] = writer.books.all()
+        return context
