63d5cfadbed8569b24419d5d14b9f8c51ce40bbf
budeikin <budeikin52@gmail.com>
2023-10-02 12:08:33 +0330

    adding list_of_books_by_genre

--- core/book/urls.py
+++ core/book/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import BookListView, BookDetailView, WritersListView,WritersDetailView
+from .views import BookListView, BookDetailView, WritersListView, WritersDetailView
 
 app_name = 'book'
 
 urlpatterns = [
     path('list/', BookListView.as_view(), name='list'),
+    path('list/<int:id>/', BookListView.as_view(), name='list_by_genre'),
     path("show/<int:pk>/", BookDetailView.as_view(), name="book_detail"),
     path('writers/', WritersListView.as_view(), name='writers_list'),
     path("writers/<slug:slug>/", WritersDetailView.as_view(), name="writer_detail"),
--- core/book/views.py
+++ core/book/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
-from .models import Book, Writer
+from .models import Book, Writer, Genre
 
 
 # Create your views here.
@@ -9,6 +9,17 @@ class BookListView(ListView):
     model = Book
     context_object_name = 'books'
     template_name = 'book/list_of_books.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.kwargs.get('id'):
+            return qs.filter(genre__id=self.kwargs.get('id'))
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['genres'] = Genre.objects.all()
+        return context
 
 
 class BookDetailView(DetailView):
