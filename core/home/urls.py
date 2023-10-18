033bb4d6b9f9b5c2c807681dbc685e0a783a3027
budeikin <budeikin52@gmail.com>
2023-10-18 12:29:32 +0330

    adding contact us

--- core/home/urls.py
+++ core/home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView,AboutUsView
+from .views import HomeView,AboutUsView,ContactView
 
 app_name = 'home'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home_page'),
     path('about-us/', AboutUsView.as_view(), name='about_us_page'),
+    path('contact-us/', ContactView.as_view(), name='contact_us_page'),
 ]
--- core/home/views.py
+++ core/home/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.core.mail import send_mail, EmailMessage
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import TemplateView, FormView
+from .forms import ContactForm
 from book.models import Writer, Book
 
 
@@ -17,3 +21,28 @@ class HomeView(TemplateView):
 
 class AboutUsView(TemplateView):
     template_name = 'home/about_us_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['form'] = ContactForm
+        return context
+
+
+class ContactView(FormView):
+    form_class = ContactForm
+    template_name = 'home/about_us_page.html'
+    success_url = 'http://127.0.0.1:8000/about-us/'
+
+    def form_valid(self, form):
+        name = form.cleaned_data['name']
+        email = form.cleaned_data['email']
+        message = form.cleaned_data['message']
+
+        email_message = EmailMessage(
+            'Contact Form Submission from {}'.format(name),
+            message,
+            email,
+            ["budeikin52@gmail.com", ],
+        )
+        email_message.send()
+        return redirect('home:about_us_page')
